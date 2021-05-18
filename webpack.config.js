2df91c6ec5342ead37d827419d91e64d3ed22ba4
icyfanfan <memory.icy@gmail.com>
2021-05-18 17:54:09 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath: "auto",
   },
 
   resolve: {
